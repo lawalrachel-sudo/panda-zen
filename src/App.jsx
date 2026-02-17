b46f0d70818cfc1fcb9e8df73678d252c01a6c5c
PANDA ZEN <lawalrachel@gmail.com>
2026-02-17 13:57:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -690,7 +690,7 @@ const BreathScreen = ({ galets, setGalets, onNav }) => {
             <>
              <div className="breath-timer-circle" style={{
                 transform: currentStep.phase.toLowerCase().includes('inspir') 
-                  ? `scale(${1 + (1 - timeLeft / currentStep.time) * 0.2})`
+                  ? `scale(${currentStepIndex > 0 && selectedExercise.steps[currentStepIndex - 1].phase.toLowerCase().includes('inspir') ? 1.1 + (1 - timeLeft / currentStep.time) * 0.1 : 1 + (1 - timeLeft / currentStep.time) * 0.2})`
                   : currentStep.phase.toLowerCase().includes('expir')
                   ? `scale(${1.2 - (1 - timeLeft / currentStep.time) * 0.2})`
                   : currentStep.phase.toLowerCase().includes('rétention') && currentStepIndex > 0 && selectedExercise.steps[currentStepIndex - 1].phase.toLowerCase().includes('inspir')
